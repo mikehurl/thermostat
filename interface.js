37c4659631f084135698d573c5580827c07a13ef
ejatkin <ejatkin@gmail.com>
2016-12-08 16:48:36 +0000

    Added additional jQuery functionality for buttons

--- interface.js
+++ interface.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.currentTemperature);
+  $('#temperature-increase').on('click', function(){
+    thermostat.increaseTemperature();
+    $('#temperature').text(thermostat.currentTemperature);
+  })
+  $('#temperature-decrease').click(function(){
+    thermostat.decreaseTemperature();
+    $('#temperature').text(thermostat.currentTemperature);
+  })
 })

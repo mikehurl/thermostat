0c4df3b131a5a2997dd131dc32b1b964e1f6dcbf
Mike Hurl <mikehurl@gmail.com>
2016-12-09 16:25:19 +0000

    integrates openweather api to display current temp in London; also allows user to select their city but we cannot display temp on this basis currently

--- interface.js
+++ interface.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   };
 
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  $('#outside-temperature').text(data.main.temp);
+    $('#outside-temperature').text(data.main.temp);
   })
 
   $('#temperature').text(thermostat.currentTemperature);
